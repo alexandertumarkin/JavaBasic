c9dd56fe2defb801228b562710a469a3513c07f2
Alex <alextumarkin@gmail.com>
2022-06-20 20:58:00 +0300

    Initial.

---
+++ 001_OOP/001_Samples/src/com/itvdn/javaEssential/ex_007_packages02_import/Main.java
@@ -0,0 +1,10 @@
+package com.itvdn.javaEssential.ex_007_packages02_import;
+
+import com.itvdn.javaEssential.ex_001_class_declaration.Car;
+
+public class Main {
+    public static void main(String[] args) {
+        // Если класс (Машина) лежит в другом пакете, то что бы его использовать в Классе Main - его надо импортировать
+        Car myCar = new Car();
+    }
+}
